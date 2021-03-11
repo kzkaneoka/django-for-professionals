e1a62f753aeb3d6aa4cd6439d38880518c685db6
Kazuki Kaneoka <kzkaneoka@gmail.com>
2021-03-11 15:17:23 +0900

    feat: integrated with bootstrap

--- books/config/settings.py
+++ books/config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third party
+    "crispy_forms",
     # Local
     "accounts",
     "pages",
@@ -142,3 +144,5 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 LOGIN_REDIRECT_URL = "home"
 
 LOGOUT_REDIRECT_URL = "home"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
